a3c85b67d4f3e6b9348b3b752631dce08a439c3a
huanghui <815189517@qq.com>
2021-10-08 14:31:52 +0800

    解决浏览器显示中文乱码问题

---
+++ server/09-node.js
@@ -0,0 +1,29 @@
+//目标：解决浏览器显示中文乱码问题
+
+//1. 加载http模块
+const http = require('http');
+
+//2. 创建服务器
+const server = http.createServer();
+
+//3. 开启服务器
+server.listen(3001, () => {
+  console.log('server is running...');
+})
+
+//4. 监听浏览器请求
+server.on('request', (req, res) => {
+  //req对象的url属性中保存了当前请求的url地址
+  // console.log(req.url);
+  const url = req.url;
+  res.setHeader('content-type', 'text/html;charset=utf-8');
+  if (url === '/') {
+    res.end('首页');
+  } else if (url === '/admin/login') {
+    res.end('登录页');
+  } else if (url === '/goods/list') {
+    res.end('列表页');
+  } else {
+    res.end('not found');
+  }
+})
